1d69f4599c901c1469bf0834b46b6084c2fc5b38
hsn6 <hsn6@openmailbox.org>
2018-01-23 13:04:50 +0330

    eta

--- Rektangulo.gd
+++ Rektangulo.gd
@@ -5,7 +5,7 @@ onready var Kasxi = get_node("Kasxi")
 func _ready():
 	pass
 
-func _on_Materialo_body_enter( korpo ):
+func _on_Rektangulo_body_enter( korpo ):
 	clear_shapes()
 	T.Radiko.Crash_14.set("stream/play", T.Agordejo.get_value("Agordoj", "Sonoj", true))
 	var Aspekto = get_node("Aspekto")
@@ -25,6 +25,6 @@ func _on_Kasxi_tween_complete( objekto, klavo ):
 	else:
 		queue_free()
 
-func _on_Materialo_area_enter( areo ):
+func _on_Rektangulo_area_enter( areo ):
 	if T.get_layer_bit(areo, 1) == true:
 		queue_free()
